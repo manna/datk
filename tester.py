4a9bba80cc0c9635255303524154951a6923590d
amin10 <manna@mit.edu>
2015-10-09 13:16:30 -0400

    Added @test decorator, with optional precision and timeout arguments

--- tester.py
+++ tester.py
@@ -1,24 +1,29 @@
 from threading import Thread
 from time import sleep
 
-TIMEOUT = 10
+TIMEOUT = 5
 PRECISION = 0.1
-def test(f, timeout=TIMEOUT, precision = PRECISION):
-    t = Thread(target = f)
-    t.daemon = True
-    t.start()
+def test(f=None, timeout=TIMEOUT, precision = PRECISION):
+    def test_decorator(f):
+        t = Thread(target = f)
+        t.daemon = True
+        t.start()
 
-    timer = 0.
-    while timer < timeout:
-        sleep(precision)
-        if not t.isAlive():
-            result = f.__name__ + " RAN IN " +str(timer) + "s"
+        timer = 0.
+        while timer < timeout:
+            sleep(precision)
+            if not t.isAlive():
+                result = f.__name__ + " RAN IN " +str(timer) + "s"
+                print result
+                print "#"*len(result)
+                break
+            timer+=precision
+
+        if t.isAlive():
+            result = f.__name__ + " TIMED OUT AFTER " + str(timeout) + "s"
             print result
             print "#"*len(result)
-            break
-        timer+=precision
-
-    if t.isAlive():
-        result = f.__name__ + " TIMED OUT AFTER " + str(timeout) + "s"
-        print result
-        print "#"*len(result)
+    if f is None:
+        return test_decorator
+    else:
+        test_decorator(f)
--- tests.py
+++ tests.py
@@ -3,24 +3,22 @@ from networks import *
 from algs import *
 from tester import *
 
-
+@test(precision = 1e-7)
 def LCR_UNIDIR_RING():
     r = Unidirectional_Ring(3)
     LCR(r)
 
+@test(precision = 1e-3)
 def ASYNC_LCR_UNIDIR_RING():
     r = Unidirectional_Ring(6)
     AsyncLCR(r)
 
+@test
 def LCR_COMPLETE():
     x = Complete_Graph(7)
     LCR(x)
 
+@test
 def ASYNC_LCR_COMPLETE():
     x = Complete_Graph(7)
     AsyncLCR(x)
-
-test(ASYNC_LCR_COMPLETE)
-test(LCR_UNIDIR_RING, precision = 1e-7)
-test(ASYNC_LCR_UNIDIR_RING, precision = 1e-3)
-test(LCR_COMPLETE)
