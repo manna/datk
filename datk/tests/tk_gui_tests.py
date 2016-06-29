18b0b44569d871d634be6fcef63b8e441395e0a0
amin10 <manna@mit.edu>
2016-06-28 20:26:40 -0700

    Added test test_tooltip_tk. Refactored tk_gui_tests

--- datk/tests/tk_gui_tests.py
+++ datk/tests/tk_gui_tests.py
@@ -12,21 +12,25 @@ $ python -m datk.tests.gui_tests """)
 
 from helpers import Artificial_LE_Network
 from mock import patch
+from Tkinter import Tk
+from datk.core.simulator_tk import draw, simulate, ToolTip
 
 Algorithm.DEFAULT_PARAMS = {"draw":False, "verbosity" : Algorithm.QUIET}
 
 def test_network_draw_tk():
-    from datk.core.simulator_tk import draw
-    from Tkinter import Tk
     with patch.object(Tk, 'mainloop'):
         x = Unidirectional_Ring(6)
         LCR(x)
         draw(x)
 
 def test_network_simulate_tk():
-    from datk.core.simulator_tk import simulate
-    from Tkinter import Tk
     with patch.object(Tk, 'mainloop'):
         x = Unidirectional_Ring(6)
         LCR(x)
         simulate(x)
+
+def test_tooltip_tk():
+    x = Unidirectional_Ring(6)
+    LCR(x)
+    x.restore_snapshot(1) #First snapshot after LCR start
+    tt = ToolTip(None, x[0], 0, 0)
